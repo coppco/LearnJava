16fcbee6f6a12ad8ad5bf975dca74cbcaba38ace
coppco <coppco@qq.com>
2016-12-04 21:42:31 +0800

    day01

---
+++ day01/EditPlus.java
@@ -0,0 +1,7 @@
+class EditPlus								//这是一个类的声明, 类名EditPlus
+{
+	public static void main(String[] args)  //这是主方法, 程序的入口
+	{
+		System.out.println("Hello World!"); //这是输出语句, 会在控制台输出一句话
+	}
+}
---
+++ day01/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("哈哈"); //println 会换行
+		System.out.print("你好");  //print不会换行
+	}
+}
+
+class Person {
+	public static void main(String[] args) {
+		System.out.println("我叫xxx");
+	}
+}
